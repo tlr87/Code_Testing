bbcc129348ff491043f7ca8f8acad09e5196a5ff
Tom Revill <tom.revill@gmail.com>
2017-07-09 22:17:54 +1200

    ForEach_ForLoop.js

--- ForEach_ForLoop.js
+++ ForEach_ForLoop.js
@@ -3,20 +3,10 @@ var LabArr =[4,9,14,19,24,29];
 var LabObjNew = {
   Num0:0
 };
-var c=0;
 var o=0;
 
-  function labFn(a,i){
+function labFn(a,i){
   return a[i]+1;
-}
-
-// LabArr.forEach(
-// function LabCounter(){
-//   console.log(c++);
-// });
-
-function LabCounter(){
-  console.log(c++);
 }
 
 // forEach
@@ -28,11 +18,12 @@ LabArr.forEach(function(){
   }
 );
 
-// for (var i = 0; i < LabArr.length; i++) {
-//   console.log(labFn(LabArr,i));
-//   var labEle = labFn(LabArr,i);
-//   LabObjNew["num"+i] = labEle;
-// }
+//forLoop
+for (var i = 0; i < LabArr.length; i++) {
+  console.log(labFn(LabArr,i));
+  var labEle = labFn(LabArr,i);
+  LabObjNew["num"+i] = labEle;
+}
 
 console.log(LabArr);
 console.log(LabObjNew);
